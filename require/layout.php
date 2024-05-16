d0fd595aa1f324d9c20f880a847f9a524aa1707f
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-05-16 11:57:29 +0200

    feat: require init

---
+++ require/layout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Exercice require</title>
+    <link rel="stylesheet" href="assets/style.css" />
+</head>
+
+<body>
+
+</body>
+
+</html>
